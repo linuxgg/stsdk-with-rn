7900d9d4a3940a418f0e60d8f5dba38a8e7970f0
Tom <qdtom@foxmail.com>
2021-09-01 22:41:50 +0800

    [UPD] Update for scan module.

--- android/app/src/main/java/com/strn/scan/ScanModule.java
+++ android/app/src/main/java/com/strn/scan/ScanModule.java
@@ -1,5 +1,6 @@
 package com.strn.scan;
 
+import android.content.Intent;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -7,6 +8,7 @@ import androidx.annotation.NonNull;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+import com.scantrust.sdk.stsdk_demo.ScanActivity;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -29,5 +31,7 @@ public class ScanModule extends ReactContextBaseJavaModule {
     @ReactMethod
     public void start() {
         Toast.makeText(getReactApplicationContext(), "scan auth", Toast.LENGTH_SHORT).show();
+        Intent i = new Intent(getCurrentActivity(), ScanActivity.class);
+        getCurrentActivity().startActivity(i);
     }
 }
---
+++ android/stsdk_demo/src/main/java/com/scantrust/sdk/stsdk_demo/ScanActivity.java
@@ -0,0 +1,15 @@
+package com.scantrust.sdk.stsdk_demo;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ScanActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d("TAG", "start scan activity");
+    }
+}
